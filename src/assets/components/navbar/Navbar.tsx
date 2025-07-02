3937193e661e2c4aac50cf8fc0c3270b9691808b
Fernanda Murched <fernanda_hco@outlook.com.br>
2025-07-02 11:36:08 -0300

    feat: cria componentes de Navbar, Footer e Home com layout inicial

---
+++ src/assets/components/navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+import { House, List, Plus } from '@phosphor-icons/react'
+import { Link } from 'react-router-dom'
+import{ Pill } from '@phosphor-icons/react'
+
+function Navbar() {
+    return (
+        <div className='w-full flex justify-center py-4 bg-indigo-900 text-white shadow-lg'>
+            <div className="container flex justify-between items-center text-lg">
+                <Link to="/" className="flex items-center gap-2 font-bold text-xl">
+                    <Pill size={32} />
+                    Health+
+                </Link>
+
+                <div className='flex gap-6'>
+                    <Link to="/" className="flex items-center gap-1 hover:text-indigo-300 transition-colors">
+                        <House size={18} />
+                        Home
+                    </Link>
+                    <Link to="/categorias" className="flex items-center gap-1 hover:text-indigo-300 transition-colors">
+                        <List size={18} />
+                        Categorias
+                    </Link>
+                    <Link to="/categorias/cadastrar" className="flex items-center gap-1 hover:text-indigo-300 transition-colors">
+                        <Plus size={18} />
+                        Cadastrar Categoria
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/assets/pages/home/Home.tsx
@@ -0,0 +1,37 @@
+function Home() {
+    return (
+        <>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4 text-center">
+                        <h2 className='text-5xl font-bold'>
+                            Boas-vindas
+                        </h2>
+                        <p className='text-xl'>
+                            Cuidando da sua saúde com atenção, qualidade e carinho.
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white border-white border-solid border-2 py-2 px-4 cursor-pointer hover:bg-white hover:text-indigo-900 transition'>
+                                Ver Produtos
+                            </div>
+                            <div className='rounded text-white border-white border-solid border-2 py-2 px-4 cursor-pointer hover:bg-white hover:text-indigo-900 transition'>
+                                Fale Conosco
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center">
+                        <img
+                            src="https://ik.imagekit.io/txvje9hzw/farmaceutico-minusculo-com-pilulas-e-vitaminas-ilustracao-vetorial-plana-medicos-prescrevendo-antibioticos-trabalhando-juntos-ajudando-pacientes-a-se-curarem-negocio-de-farmacia-conceito-de-farmacia.png?updatedAt=1751462182138"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
